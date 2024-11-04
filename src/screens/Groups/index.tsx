66211ad9adef5993819d7a04dd81b5ee76d5c6b5
C1329353 Abner Borda Fonseca <c1329353@interno.bb.com.br>
2024-11-03 22:35:25 -0300

    Adicionado imports

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { Container, TextStyled } from "./styles";
+
+export default function Groups() {
+  return (
+    <Container>
+      <TextStyled>Groups</TextStyled>
+    </Container>
+  );
+}
---
+++ src/screens/Groups/styles.ts
@@ -0,0 +1,15 @@
+import { Text } from "react-native";
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: #f5f5f5;
+  justify-content: center;
+  align-items: center;
+  background-color: #000;
+`;
+
+export const TextStyled = styled.Text`
+  font-size: 24px;
+  color: red;
+`;
